29e7cf611f75b09447daa0f0ec518ef4795a2ba7
Vinodh Periyasamy <vinodh.periyasamy@jet.com>
2018-06-13 17:26:40 -0400

    minor fixes

--- python/a_sample_scrape.py
+++ python/a_sample_scrape.py
@@ -21,8 +21,6 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
-i = 1
-deal_products = []
 
 t0 = time.time()
 
